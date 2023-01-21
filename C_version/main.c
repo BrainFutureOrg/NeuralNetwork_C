32e3fc750c360f0d62c471bf40937093b01e368a
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-01-21 14:21:48 +0200

    matrix operetions with some test

--- C_version/main.c
+++ C_version/main.c
@@ -1,6 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "matrix_operations.h"
+#include <errno.h>
+
+void check_matrix_print();
+
+void check_matrix_multiplication();
 
 int main() {
-    printf("Hello, World!\n");
+
+//    printf("Hello, World!\n");
+    check_matrix_multiplication();
+//    check_matrix_print();
     return 0;
 }
+
+void check_matrix_print() {
+
+    double matrix1[3][2] = {{1, 2},
+                            {3, 4},
+                            {5, 6}};
+    int size1 = sizeof(matrix1) / sizeof(matrix1[0]);
+    int size2 = sizeof(matrix1[0]) / sizeof(matrix1[0][0]);
+    double **matrix_pointer;
+    matrix_pointer = calloc(size1, sizeof(double *));
+    for (int i = 0; i < size1; ++i) {
+        matrix_pointer[i] = calloc(size2, sizeof(double));
+    }
+    for (int i = 0; i < size1; ++i) {
+        for (int j = 0; j < size2; ++j) {
+            matrix_pointer[i][j] = matrix1[i][j];
+        }
+    }
+    matrix checking;
+    checking.table = matrix_pointer;
+    checking.i = size1;
+    checking.j = size2;
+
+    matrix_print(checking);
+//    matrix_free(checking);
+    matrix checkingT = matrix_transposition(checking);
+    matrix_print(checkingT);
+    matrix_free(checking);
+    matrix_free(checkingT);
+}
+
+void check_matrix_multiplication() {
+    double m[2][3] = {{1, 2, 5},
+                      {3, 4, 6}};
+    int size1 = sizeof(m) / sizeof(m[0]);
+    int size2 = sizeof(m[0]) / sizeof(m[0][0]);
+    double **matrix_pointer;
+    matrix_pointer = calloc(size1, sizeof(double *));
+    for (int i = 0; i < size1; ++i) {
+        matrix_pointer[i] = calloc(size2, sizeof(double));
+    }
+    for (int i = 0; i < size1; ++i) {
+        for (int j = 0; j < size2; ++j) {
+            matrix_pointer[i][j] = m[i][j];
+        }
+    }
+    matrix checking;
+    checking.table = matrix_pointer;
+    checking.i = size1;
+    checking.j = size2;
+    matrix matr = matrix_multiplication(checking, checking);
+    if (errno == 0) {
+        matrix_print(matr);
+    } else {
+        printf("error");
+        matrix_free(checking);
+        return;
+    }
+    matrix_free(matr);
+    matrix_free(checking);
+}
+
--- C_version/matrix_operations.h
+++ C_version/matrix_operations.h
@@ -4,5 +4,18 @@
 
 #ifndef C_VERSION_MATRIX_OPERATIONS_H
 #define C_VERSION_MATRIX_OPERATIONS_H
+typedef struct matrix{
+    double **table;
+    int i;
+    int j;
+} matrix;
+
+matrix matrix_multiplication(matrix first_matrix, matrix second_matrix);
+
+matrix matrix_transposition(matrix matrix_to_transpose);
+
+void matrix_print(matrix matrix_to_print);
+
+void matrix_free(matrix matrix_to_free);
 
 #endif //C_VERSION_MATRIX_OPERATIONS_H
