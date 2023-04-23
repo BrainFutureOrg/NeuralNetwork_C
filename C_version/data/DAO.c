a0bb2edfe9e1b65e715cc8b8e6b36b1e81ca085e
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-04-23 16:03:37 +0300

    Data reader

--- C_version/data/DAO.c
+++ C_version/data/DAO.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "DAO.h"
+#include "errno.h"
 
 void readline(char *file_name) {
     FILE *file_csv = fopen(file_name, "r");
@@ -32,4 +33,41 @@ double *get_line_matrix(FILE *file) {
         getc(file);
     }
     return numbers;
+}
+
+data_reader
+create_data_reader(char *file_name, int from_line, int sample_number, int batch_size, void (*data_prepare)(matrix)) {
+    FILE *fp = open_file(file_name);
+    pass_line(fp); // pass header
+    for (int i = 0; i < from_line; ++i) {
+        pass_line(fp);
+    }
+    return (data_reader) {fp, data_prepare, sample_number, batch_size, 0};
+}
+
+matrix **read_batch_from_data_nn(data_reader *reader) {
+    int delta = reader->sample_number - reader->this_elem;
+    if (delta < 0) {
+        printf("ERROR, ");
+        errno = EDOM;
+    }
+    int sample_number = reader->batch_size < delta ? reader->batch_size : delta;
+    matrix **data = calloc(2, sizeof(matrix *));
+    matrix *answers = calloc(sample_number, sizeof(matrix));
+    matrix *for_predict = calloc(sample_number, sizeof(matrix));
+    data[1] = answers;
+    data[0] = for_predict;
+    for (int w = 0; w < sample_number; w++) {
+        double *numbers = get_line_matrix(reader->fp);
+        for_predict[w] = make_matrix_from_array(&numbers[1], 28 * 28, 1);
+        reader->data_prepare(for_predict[w]);
+        answers[w] = create_vector(10, (int) numbers[0]);
+        free(numbers);
+    }
+    reader->this_elem += sample_number;
+    return data;
+}
+
+void close_data_reader(data_reader reader) {
+    fclose(reader.fp);
 }
--- C_version/data/DAO.h
+++ C_version/data/DAO.h
@@ -6,6 +6,24 @@
 #define C_VERSION_DAO_H
 
 #include <stdio.h>
+#include "../math/matrix_operations.h"
+
+typedef struct data_reader {
+    FILE *fp;
+
+    void (*data_prepare)(matrix);
+
+    int sample_number;
+    int batch_size;
+    int this_elem;
+} data_reader;
+
+data_reader
+create_data_reader(char *file_name, int from_line, int sample_number, int batch_size, void (*data_prepare)(matrix));
+
+matrix **read_batch_from_data_nn(data_reader *reader);
+
+void close_data_reader(data_reader reader);
 
 void readline(char *file_name);
 
